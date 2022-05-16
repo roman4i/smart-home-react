fb2097a7d37b5d600385f1df21cd9086e6043432
roman4i <verb.r17@gmail.com>
2022-05-16 14:19:35 +0300

    added footer (for logging)

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Header from "../../views/Header/Header";
 import Navigation from "../Navigation/Navigation";
 import HomePage from "../../views/Home/HomePage.";
+import Footer from "../../views/Footer/Footer";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route path="settings" element={<div>Settings</div>} />
         <Route path="*" element={<div>Error</div>} />
       </Routes>
+      <Footer />
     </div>
   );
 }
